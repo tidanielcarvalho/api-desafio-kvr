ba70a6cbf404765778372e48c860cc175f708076
Daniel Carvalho <ti.danielcarvalho@gmail.com>
2022-06-28 20:16:59 -0300

    feat: criado config do redis junto com suas queries para cache

---
+++ repositories/redis/config.go
@@ -0,0 +1,98 @@
+package redis
+
+import (
+	"api-desafio-kvr/helpers"
+	"api-desafio-kvr/models"
+	"encoding/json"
+
+	"github.com/go-redis/redis"
+)
+
+var logger = &helpers.Log{}
+var YesDeleteAll = true
+var NoDeleteAll = false
+var PrefixDeleteAll = "ListAll"
+var nameLog = "REDIS"
+
+func Connect() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return rdb
+}
+
+func Get(key string) []byte {
+	logger.Debug(nameLog, "Getting cache for key: "+key)
+
+	client := Connect()
+	result, err := client.Get(key).Result()
+	if err != nil {
+		logger.Error(key, err.Error())
+	}
+
+	if result == "" {
+		return nil
+	}
+
+	return []byte(result)
+}
+
+func Set(key string, crypto models.CryptoCurrency, deleteAll bool) error {
+	logger.Debug(nameLog, "Setting cache for key: "+key)
+
+	byteValue, err := json.Marshal(crypto)
+	if err != nil {
+		logger.Error(crypto.Id.Hex(), "Error in response: "+err.Error())
+		return err
+	}
+
+	client := Connect()
+	err = client.Set(key, string(byteValue), 0).Err()
+
+	if deleteAll {
+		err = DeleteAll()
+	}
+
+	return err
+}
+
+func SetByByte(key string, value string, deleteAll bool) error {
+	logger.Debug(nameLog, "Setting cache for key: "+key)
+	client := Connect()
+	err := client.Set(key, value, 0).Err()
+
+	if deleteAll {
+		err = DeleteAll()
+	}
+
+	return err
+}
+
+func Del(key string) error {
+	logger.Debug(nameLog, "Deleting cache for key: "+key)
+
+	client := Connect()
+	err := client.Del(key).Err()
+
+	return err
+}
+
+func DeleteAll() error {
+	logger.Debug(nameLog, "Deleting cache for "+PrefixDeleteAll)
+
+	client := Connect()
+	iter := client.Scan(0, PrefixDeleteAll+"*", 0).Iterator()
+	for iter.Next() {
+		err := client.Del(iter.Val()).Err()
+		if err != nil {
+			logger.Error(nameLog, err.Error())
+		}
+	}
+
+	err := iter.Err()
+
+	return err
+}
